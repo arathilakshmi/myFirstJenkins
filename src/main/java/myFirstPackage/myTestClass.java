a2459c8af64f3322a836905d77989793e98c506c
arathilakshmi <arathilakshmi.de@gmail.com>
2021-09-07 11:20:06 +0200

    My First Jenkins/Java commit

---
+++ src/main/java/myFirstPackage/myTestClass.java
@@ -0,0 +1,7 @@
+package myFirstPackage;
+
+public class myTestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello ! This is my first Java class in years..... !!");
+    }
+}
