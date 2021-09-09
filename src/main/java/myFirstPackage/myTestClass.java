6e57f970f49ed3593d270b1a2aab7f6e8e82b50a
arathilakshmi <arathilakshmi.de@gmail.com>
2021-09-09 11:32:27 +0200

    Added a new method

--- src/main/java/myFirstPackage/myTestClass.java
+++ src/main/java/myFirstPackage/myTestClass.java
@@ -1,7 +1,12 @@
 package myFirstPackage;
 
 public class myTestClass {
+    public static String testMethod( int num ) {
+        return "I got "+num;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello ! This is my first Java class in years..... !!");
+        System.out.println(testMethod(8));
     }
 }
